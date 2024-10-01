005a56bd956146ea956d7e4414211232f06cecf4
sina-byn <sina.sina.bayan@protonmail.com>
2024-10-01 09:05:35 +0330

    applyVars util added

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,13 @@ import { fileURLToPath } from 'url';
 export const __dirname = () => dirname(fileURLToPath(import.meta.url));
 
 export const MD5Hash = input => crypto.createHash('md5').update(input).digest('hex');
+
+export const applyVars = (input, vars = {}) => {
+  const varRegex = /<(\w+)>/g;
+
+  return input.replace(varRegex, (_, varName) => {
+    if (!(varName in vars)) throw new Error(`Could Not Resolve Variable '${varName}'`);
+
+    return vars[varName];
+  });
+};
